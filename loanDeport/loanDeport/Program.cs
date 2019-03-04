f971f1cd3bb267e24fc38d5fca8828138a55c704
thenmoli <ttkugan@gmail.com>
2019-03-04 11:44:02 -0800

    Add files via upload

---
+++ loanDeport/loanDeport/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loanDeport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            total = Enumerable.Range(0, 1000).Where(x => x % 3 == 0 || x % 5 == 0).Sum();
+            Console.WriteLine("The sum is : {0}\n", total);
+            Console.ReadLine();
+        }
+    }
+}
